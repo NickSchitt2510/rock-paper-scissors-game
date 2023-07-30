7a3a144dfd38ad6ff30c0133d32b7083404c10ba
NickSchitt2510 <121604402+NickSchitt2510@users.noreply.github.com>
2023-07-30 22:39:38 +0800

    Reorganize script.js

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-// Change number to choice
-function numberToChoice(number) {
-    switch (number) {
-        case 0:
-            return "✊";
-        case 1:
-            return "✋";
-        case 2:
-            return '✌️';
+// Initialize score
+let playerScore = 0;
+let computerScore = 0;
+
+// Add a start button to start the game
+function startGame() {
+
+    // Transition to playing ui after clicking start button
+    const start = document.querySelector('.start-game');
+    start.addEventListener('click', () => {
+        start.setAttribute('hidden', true);
+        document.getElementById('play-game').removeAttribute('hidden');
+    });
+
+    // Set up event listener. Click either button to playRound.
+    const choices = document.querySelectorAll('.choices'); // return a node list
+    choices.forEach((choice) => {
+        choice.addEventListener('click', playRound);
+    });
+}
+
+
+function playRound() {
+    // Get the value of player's choice
+    let playerSelection = +this.value;
+    let computerSelection = generateComputerSelection();
+
+    // Play round and compare the selection of player and computer
+    let result = compareSelection(playerSelection, computerSelection);
+    
+    // Display selections of both players using emoji
+    const player = document.querySelector('.player');
+    player.textContent = numberToChoice(playerSelection);
+    
+    const computer = document.querySelector('.computer');
+    computer.textContent = numberToChoice(computerSelection)
+
+    // Display round result and score
+    const roundResult = document.querySelector('.result');
+    roundResult.textContent = `${result}`;
+
+    const score = document.querySelector('.score');
+    score.textContent = `${playerScore} - ${computerScore}`;
+    
+    // if either score reaches 5, announce the winner
+    if (playerScore === 5 || computerScore === 5) {
+        announceWinner();
     }
 }
 
@@ -27,32 +65,32 @@ function generateComputerSelection() {
 
 
 function compareSelection(playerSelection, computerSelection) {
-//computerSelection and playerSelection are numbers
-
-    if (playerSelection === computerSelection) {
-        return "Draw";
+    //computerSelection and playerSelection are numbers
+    
+        if (playerSelection === computerSelection) {
+            return "Draw";
+        }
+        else if (playerSelection === 0 && computerSelection === 2 || playerSelection === 1 && computerSelection === 0 || playerSelection === 2 && computerSelection === 1) {
+            playerScore++;
+            return "You Won This Round";
+        }
+        else if (playerSelection === 0 && computerSelection === 1 || playerSelection === 1 && computerSelection === 2 || playerSelection === 2 && computerSelection === 0) {
+            computerScore++;
+            return "Computer Won This Round";
+        }
     }
-    else if (playerSelection === 0 && computerSelection === 2 || playerSelection === 1 && computerSelection === 0 || playerSelection === 2 && computerSelection === 1) {
-        playerScore++;
-        return "You Won This Round";
+
+
+// Change number to choice
+function numberToChoice(number) {
+    switch (number) {
+        case 0:
+            return "✊";
+        case 1:
+            return "✋";
+        case 2:
+            return '✌️';
     }
-    else if (playerSelection === 0 && computerSelection === 1 || playerSelection === 1 && computerSelection === 2 || playerSelection === 2 && computerSelection === 0) {
-        computerScore++;
-        return "Computer Won This Round";
-    }
-}
-
-
-// Initialize score
-let playerScore = 0;
-let computerScore = 0;
-
-
-function disableButtons() {
-    const choices = document.querySelectorAll('.choices');
-    choices.forEach((choice) => {
-        choice.disabled = true;
-    })
 }
 
 
@@ -68,7 +106,6 @@ function announceWinner() {
     section.appendChild(ending);
 
     const result = document.querySelector('.ending');
-    
     if (playerScore > computerScore) {
         result.textContent = "You Won the Game!";
         disableButtons();
@@ -77,7 +114,19 @@ function announceWinner() {
         disableButtons();
     }
 
-    // Add play again button
+    addPlayAgainButton();
+}
+
+
+function disableButtons() {
+    const choices = document.querySelectorAll('.choices');
+    choices.forEach((choice) => {
+        choice.disabled = true;
+    })
+}
+
+
+function addPlayAgainButton() {
     const button = document.createElement('button');
     button.classList.add('function-btn');
     button.textContent = 'Play Again';
@@ -88,51 +137,31 @@ function announceWinner() {
     againButton.addEventListener('click', restartGame);
 }
 
-// When button is clicked, get the value of player's choice and get result
-function playRound() {
-    let playerSelection = +this.value;
-    let computerSelection = generateComputerSelection();
-    let result = compareSelection(playerSelection, computerSelection);
+
+function restartGame() {
+    // Undo all elements changes
+    removeAddedElements();
+    document.querySelector('.score').textContent = '';
+    document.querySelector('.result').textContent = '';
+
+    // Set up event listener for choices buttons.
+    const choices = document.querySelectorAll('.choices');
     
-    // Display selections of both players using emoji
-    const player = document.querySelector('.player');
-    player.textContent = numberToChoice(playerSelection);
-    
-    const computer = document.querySelector('.computer');
-    computer.textContent = numberToChoice(computerSelection)
+    // Reset buttons
+    choices.forEach((choice) => {
+        choice.disabled = false;
+    })
 
-    // Display round result
-    const roundResult = document.querySelector('.result');
-    roundResult.textContent = `${result}`;
+    // Reset scores
+    playerScore = 0;
+    computerScore = 0;
 
-    const score = document.querySelector('.score');
-    score.textContent = `${playerScore} - ${computerScore}`;
-    
-    // if either score reaches 5, announce the winner
-    if (playerScore === 5 || computerScore === 5) {
-        
-        announceWinner();
-    }
-}
-
-
-// add a start button to start the game
-function startGame() {
-
-    // transition to playing ui after clicking start button
-    const start = document.querySelector('.start-game');
-    start.addEventListener('click', () => {
-        start.setAttribute('hidden', true);
-        document.getElementById('play-game').removeAttribute('hidden');
-    });
-
-    // Set up event listener. Click either button to playRound.
-    const choices = document.querySelectorAll('.choices'); // return a node list
+    // Click either button to playRound.
     choices.forEach((choice) => {
         choice.addEventListener('click', playRound);
     });
-    
 }
+
 
 function removeAddedElements() {
     const game = document.querySelector('.game');
@@ -146,32 +175,5 @@ function removeAllChild(parent) {
     }
 }
 
-function restartGame() {
-    // undo all elements changes
-    removeAddedElements();
-    
-    document.querySelector('.score').textContent = '';
-    document.querySelector('.result').textContent = '';
-
-    // Set up event listener for buttons.
-    const choices = document.querySelectorAll('.choices');
-    
-    // Reset buttons
-    choices.forEach((choice) => {
-        choice.disabled = false;
-    })
-
-    // reset scores
-    playerScore = 0;
-    computerScore = 0;
-
-    // Click either button to playRound.
-    choices.forEach((choice) => {
-        choice.addEventListener('click', playRound);
-    });
-}
-
 
 startGame();
-
-// game();
